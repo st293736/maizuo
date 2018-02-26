4f50b159923bbf686f40a46cfdb8c8eaea3c6a39
st293736 <1757279861@qq.com>
2018-02-26 11:58:30 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>这是一个简单的开始</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
