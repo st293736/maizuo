cb95a8386e6ab01dacb13697f7fa24d6b52bc247
st293736 <1757279861@qq.com>
2018-03-03 17:50:21 +0800

    周五

--- src/resource/Header.js
+++ src/resource/Header.js
@@ -56,49 +56,5 @@ const mapDispatchToProps = function(dispatch,props){
     }
 }
 const Header = connect(mapStateToProps,mapDispatchToProps)(UI);
-/*class Header extends  Component {
-    constructor(){
-        super();
-        this.state = {
-            showFlag:false
-        }
-        this.showMenu = this.showMenu.bind(this);
-    }
-    showMenu(){
-        if(this.state.showFlag){
-            this.refs.menu.style.display = "block";
-        }else {
-            this.refs.menu.style.display = "none";
-        }
-        this.setState({
-            showFlag: !this.state.showFlag,
-        })
-    }
-    render(){
-        return(
-            <div className="boxHead">
-                <header>
-                    <div className='left'>
-                        <a onClick={this.showMenu} className="iconfont icon-menu"></a>
-                        <span className="titleName">卖座电影</span>
-                    </div>
-                    <div className='right'>
-                        <Link to='/selectCity'>北京<span className="iconfont icon-jiantou"></span></Link>
-                        <Link to='/login' className="iconfont icon-icon-test"></Link>
-                    </div>
-                </header>
-                <div className="menu" ref="menu">
-                    <ul>
-                        <li><Link to="/home"><span>首页</span><span className="iconfont icon-jiantou"></span></Link></li>
-                        <li><Link to='/filmDetail/hitPlaying'><span>影片</span><span className='iconfont icon-jiantou'></span></Link></li>
-                        <li><Link to='/cinemaZone'><span>影院</span><span className='iconfont icon-jiantou'></span></Link></li>
-                        <li><a href=""><span>商城</span><span className='iconfont icon-jiantou'></span></a></li>
-                        <li><a href=""><span>我的</span><span className='iconfont icon-jiantou'></span></a></li>
-                        <li><Link to='/cart/maizuoka'><span>卖座卡</span><span className='iconfont icon-jiantou'></span></Link></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
-}*/
+
 export default Header;
